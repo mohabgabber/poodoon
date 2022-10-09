d4c840d37a0fe985eb6a5f911414e1a2e27c1212
Mohab Gabber <mohabgabber1@protonmail.com>
2022-10-09 02:36:45 +0200

    added sha512 cracker

--- script.py
+++ script.py
@@ -1,27 +1,82 @@
 import socket
 import os
 import sys
+import hashlib
 
 
 class Networking:
-    def BannerGrabb(ip, port):
+    def BannerGrab(ip, port):
         socket.setdefaulttimeout(2)
         s = socket.socket()
         try:
             s.connect((str(ip), port))
             ban = s.recv(1024)
-            return str(ban)
+            return f"[*] The Returned Banner:\n {str(ban)}\n\n"
         except Exception as e:
             return f"[-] Error = {str(e)}"
 
 
-class Cryptography:
-    def PasswordCracker(hash, wordlist):
-        pass
+class Crypto:
+    def Sha512Cracker(hash, wordlist):
+        if not os.path.isfile(wordlist):
+            return f"The File {wordlist}\n Doesn't Exist!"
+        if not os.access(wordlist, os.R_OK):
+            return f"The File {wordlist}\n Is Not Readable"
+        wlist = open(wordlist, 'r')
+        for word in wlist.readlines():
+            w = word.strip("\n")
+            hashsum = hashlib.sha512(w.encode("utf-8")).hexdigest()
+            if hashsum == hash:
+                return f"\n\n[*] Found Password: {w}\n"
+                break
+        print("\n\n[-] Password Not Found.\n")
+
+
+def cryptointro():
+    while True:
+        print('''
+        Available Tools
+            0 - Sha512 Cracker
+            100 - Back To Main Menu
+    ''')
+        choice = int(input("Choose The Desired Tools' Number: "))
+        if choice == 0:
+            hash = input("Please Paste The Hash To Crack: ")
+            wordlist = input("Please Enter The Full Path To The Wordlist: ")
+            print(Crypto.Sha512Cracker(hash, wordlist))
+        elif choice == 100:
+            break
+
+
+def networkintro():
+    while True:
+        print('''
+        Available Tools
+            0 - Banner Grab
+            100 - Back To Main Menu
+        ''')
+        choice = int(input("Choose The Desired Tools' Number: "))
+        if choice == 0:
+            ip = input("Please Enter Ip: ")
+            port = int(input("Please Enter Port: "))
+            print(f"\n\n{Networking.BannerGrab(ip, port)}")
+        elif choice == 100:
+            break
 
 
 def main():
-    pass
+    while True:
+        print("Please Press CTRL + C To Exit The Script")
+        print('''
+        Available Modules 
+            0 - Cryptography 
+            1 - Networking 
+        ''')
+        choice = int(input("Insert The Desired Modules' Number: "))
+        if choice == 0:
+            cryptointro()
+        elif choice == 1:
+            networkintro()
 
 
 if __name__ == "__main__":
